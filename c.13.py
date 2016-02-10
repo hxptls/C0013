fd2497c90c8b86dda1005d31a434be17884f983f
Hexapetalous <hexapetalous@126.com>
2016-02-10 12:19:58 +0800

    Init.

---
+++ c.13.py
@@ -0,0 +1,19 @@
+#
+# c.13.py by Hexapetalous on Feb 10, 2016.
+# This is a part of C0013. Copyright 2016 Hexapetalous. All rights reserved.
+#
+import P0009.btbb as p9b
+import P0009.btbbc as p9c
+
+
+def printl(string):
+    print(string, end='', flush=True)
+
+# STEP 1 API(s) #
+DATA_FILE_PATH = '/home/wan/233.txt'  # Default path
+RESULT_DIR_PATH = '/home/wan/N_E_I_H version1.0/C0013/result/'  # Default path
+
+# STEP 2 Parser #
+# STEP 3 Analyze file #
+# STEP 4 Fetch #
+# STEP 5 Write file #
